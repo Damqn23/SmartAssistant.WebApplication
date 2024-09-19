09f48d22ea73b333a7ac32d6248b89b2736d043d
Damqn23 <rauga2005@gmail.com>
2024-09-19 23:51:18 +0300

    ReminderService added

---
+++ SmartAssistant.Shared/Interfaces/IReminderService.cs
@@ -0,0 +1,21 @@
+﻿using SmartAssistant.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAssistant.Shared.Interfaces
+{
+	public interface IReminderService
+	{
+		Task<IEnumerable<ReminderModel>> GetAllRemindersAsync();
+		Task<ReminderModel> GetReminderByIdAsync(int id);
+		Task AddReminderAsync(ReminderModel reminder);
+		Task UpdateReminderAsync(ReminderModel reminder);
+		Task DeleteReminderAsync(int id);
+		Task UpdateReminderStatusAsync(int reminderId, bool status);
+		Task<List<ReminderModel>> GetRemindersByUserIdAsync(string userId);
+
+	}
+}
