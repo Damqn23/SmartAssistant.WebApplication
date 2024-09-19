d3ba32209a2ea8fe0e2625f13146c6d6bab676cb
Damqn23 <rauga2005@gmail.com>
2024-09-19 23:51:29 +0300

    Mapping class added

---
+++ SmartAssistant.Shared/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SmartAssistant.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAssistant.Shared.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<ReminderModel, ReminderModel>()
+                .ForMember(dest => dest.UserId, opt => opt.Ignore());
+        }
+    }
+}
