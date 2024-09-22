922cdbeaba3b5124c53c6b5447b679c0f2bb9066
Damqn23 <rauga2005@gmail.com>
2024-09-22 12:04:12 +0300

    Mapper updated

--- SmartAssistant.Shared/Mapping/MappingProfile.cs
+++ SmartAssistant.Shared/Mapping/MappingProfile.cs
@@ -11,12 +11,23 @@ namespace SmartAssistant.Shared.Mapping
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() 
+        public MappingProfile()
         {
             CreateMap<Reminder, ReminderModel>()
                 .ForMember(dest => dest.User, opt => opt.MapFrom<UserResolver>());
-            CreateMap<ReminderModel, Reminder>();
+            CreateMap<ReminderModel, Reminder>()
+              .ForMember(dest => dest.User, opt => opt.Ignore());
+            
             CreateMap<ReminderCreateModel, ReminderModel>();
+
+            CreateMap<ReminderModel, ReminderEditModel>();
+            CreateMap<ReminderEditModel, ReminderModel>()
+             .ForMember(dest => dest.UserId, opt => opt.Ignore());
+
+
+            CreateMap<ReminderModel, ReminderDeleteModel>();
+
+            CreateMap<UserModel, User>();
         }
     }
 }
